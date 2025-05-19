37682f1583e4055c1012e30b8041b0167f5e8e82
Clon54 <clon5454@outlook.com>
2025-05-18 22:38:44 -0500

    Agregue en una columna lo botones relacionados con los vendedores.

--- src/view_admin.py
+++ src/view_admin.py
@@ -11,15 +11,44 @@ def view_general(page: ft.Page) -> None:
 
     # row = ft.row()
 
-    buttom_watch_seller = ft.ElevatedButton("Ver vendedores")
-    buttom_create_seller = ft.ElevatedButton("Crear vendedores")
-    buttom_create_production = ft.ElevatedButton("Crear producto")
-    buttom_watch_orders = ft.ElevatedButton("Ver pedidos")
+    buttom_watch_seller = ft.Container(content=ft.Text("Ver vendedores"), margin=10,
+                                       padding=10,
+                                       alignment=ft.alignment.center,
+                                       bgcolor=ft.Colors.AMBER,
+                                       width=150,
+                                       height=150,
+                                       border_radius=10)
+
+    buttom_create_seller = ft.Container(
+        content=ft.Text("Crear vendedores"), margin=10,
+        padding=10,
+        alignment=ft.alignment.center,
+        bgcolor=ft.Colors.BLUE,
+        width=150,
+        height=150,
+        border_radius=10)
+
+    buttom_create_production = ft.Container(
+        content=ft.Text("Crear producto"), margin=10,
+        padding=10,
+        alignment=ft.alignment.center,
+        bgcolor=ft.Colors.ORANGE,
+        width=150,
+        height=150,
+        border_radius=10)
+
+    buttom_watch_orders = ft.Container(content=ft.Text("Ver pedidos"), margin=10,
+                                       padding=10,
+                                       alignment=ft.alignment.center,
+                                       bgcolor=ft.Colors.YELLOW,
+                                       width=150,
+                                       height=150,
+                                       border_radius=10)
+
+    row = ft.row([buttom_create_seller, buttom_watch_orders])
 
     page.add(buttom_create_production)
-    page.add(buttom_create_seller)
+
     page.add(buttom_watch_orders)
-    page.add(buttom_watch_orders)
-    page.add(buttom_watch_seller)
 
     page.update()
