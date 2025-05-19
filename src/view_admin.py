97ae8a44b9ef803597362d67f6b93d2a44e50dcf
Clon54 <clon5454@outlook.com>
2025-05-18 22:51:59 -0500

    Centre el text de panel de administrador.

--- src/view_admin.py
+++ src/view_admin.py
@@ -10,6 +10,8 @@ import flet as ft
 def view_general(page: ft.Page) -> None:
 
     # row = ft.row()
+    text = ft.Text("Panel de administrador",
+                   color=ft.Colors.BLACK, text_align=ft.TextAlign.CENTER, size=40, style=ft.TextAlign.CENTER)
 
     buttom_watch_seller = ft.Container(content=ft.Text("Ver vendedores"), margin=10,
                                        padding=10,
@@ -37,6 +39,15 @@ def view_general(page: ft.Page) -> None:
         height=150,
         border_radius=10)
 
+    buttom_watch_production = ft.Container(
+        content=ft.Text("Ver productos"), margin=10,
+        padding=10,
+        alignment=ft.alignment.center,
+        bgcolor=ft.Colors.ORANGE_700,
+        width=150,
+        height=150,
+        border_radius=10)
+
     buttom_watch_orders = ft.Container(content=ft.Text("Ver pedidos"), margin=10,
                                        padding=10,
                                        alignment=ft.alignment.center,
@@ -45,9 +56,14 @@ def view_general(page: ft.Page) -> None:
                                        height=150,
                                        border_radius=10)
 
-    row = ft.row([buttom_create_seller, buttom_watch_orders])
+    row = ft.Row([buttom_create_seller, buttom_watch_seller])
+    row_2 = ft.Row([buttom_create_production, buttom_watch_production])
 
-    page.add(buttom_create_production)
+    row_3 = ft.Row([text], alignment=ft.MainAxisAlignment.CENTER)
+
+    page.add(row_3)
+    page.add(row)
+    page.add(row_2)
 
     page.add(buttom_watch_orders)
 
