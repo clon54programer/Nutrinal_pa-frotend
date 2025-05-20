99e1869c0a81b8e242ef95122dbe016c17abe652
Clon54 <clon5454@outlook.com>
2025-05-19 21:05:22 -0500

    Implementado el codigo.

--- src/view_seller.py
+++ src/view_seller.py
@@ -1,10 +1,126 @@
 import flet as ft
+import requests
 
 
 def view_general_seller(page: ft.Page):
 
     text = ft.Text("Vendedor",
                    color=ft.Colors.BLACK, text_align=ft.TextAlign.CENTER, size=40, style=ft.TextAlign.CENTER)
+
+    def create_order(e):
+
+        print()
+        page.remove(row_buttom)
+        page.remove(row_3)
+
+    def view_watch_order(e):
+        # "data":{
+        # "identifier_client": ""
+        # "identifier_seller": "",
+        # "code_product":"",
+        # "cant_product":{},
+        # "shipping_destination": ""
+        page.remove(row_buttom)
+        page.remove(row_3)
+
+        text = ft.Text("Pedidos",
+                       color=ft.Colors.BLACK, text_align=ft.TextAlign.CENTER, size=40, style=ft.TextAlign.CENTER)
+
+        r = requests.get("http://127.0.0.1:8000/nutrinal_pa/admin/get_orders")
+
+        json = r.json()
+
+        data = json.get("data", {})
+
+        items: dict = dict(data.items())
+        index: int = 0
+
+        """
+        "id": "492e9566-7848-4c7e-bad6-90be4b24dd79",
+        "cant_product": 0,
+        "status": "pending",
+        "shipping_destination": "",
+        "seller": "Carlos",
+        "client": "Juan Pérez",
+        "product": [],
+        "order_date": "2025-05-06T03:52:35.211Z",
+        "date_update": "2025-05-06T03:52:35.211Z"
+        }"""
+
+        id = data[f"order_{index}"]['id']
+        cant_product = data[f"order_{index}"]['cant_product']
+        status = data[f"order_{index}"]['status']
+        shipping_destination = data[f"order_{index}"]['shipping_destination']
+        seller = data[f"order_{index}"]['seller']
+        client = data[f"order_{index}"]['client']
+        order_date = data[f"order_{index}"]['order_date']
+        date_update = data[f"order_{index}"]['date_update']
+
+        text_id = ft.Text(f"Id: {id}")
+        text_cant = ft.Text(f"cantidad: {cant_product}"
+                            )
+        text_status = ft.Text(f"Status: {status}")
+        text_destination = ft.Text(f"Destino: {shipping_destination}")
+        text_client = ft.Text(f"Cliente: {client}")
+        text_seller = ft.Text(f"Vendedor: {seller}")
+        text_order_date = ft.Text(f"Fecha de realicion: {order_date}")
+        text_date_update = ft.Text(f"Fecha de atualizacion: {date_update}")
+
+        col = ft.Column([text_id, text_cant, text_status, text_destination,
+                        text_client, text_seller, text_order_date, text_date_update])
+
+        def on_regret(e):
+            nonlocal index
+            if index != 0:
+                id = data[f"order_{index}"]['id']
+        cant_product = data[f"order_{index}"]['cant_product']
+        status = data[f"order_{index}"]['status']
+        shipping_destination = data[f"order_{index}"]['shipping_destination']
+        seller = data[f"order_{index}"]['seller']
+        client = data[f"order_{index}"]['client']
+        order_date = data[f"order_{index}"]['order_date']
+        date_update = data[f"order_{index}"]['date_update']
+
+        text_id = ft.Text(f"Id: {id}")
+        text_cant = ft.Text(f"cantidad: {cant_product}"
+                            )
+        text_status = ft.Text(f"Status: {status}")
+        text_destination = ft.Text(f"Destino: {shipping_destination}")
+        text_client = ft.Text(f"Cliente: {client}")
+        text_seller = ft.Text(f"Vendedor: {seller}")
+        text_order_date = ft.Text(f"Fecha de realicion: {order_date}")
+        text_date_update = ft.Text(f"Fecha de atualizacion: {date_update}")
+
+        page.update()
+
+        def on_next(e):
+            nonlocal index
+            if index < len(items) - 1:
+                index += 1
+
+                name = items[f"product_{index}"]["name"]
+                code = items[f"product_{index}"]["code"]
+                price = items[f"product_{index}"]["price"]
+                description = items[f"product_{index}"]["description"]
+
+                json_code = {"code": code}
+                r_2 = requests.post(
+                    "http://127.0.0.1:8000/nutrinal_pa/admin/get_cant_product", json=json_code)
+                cant_avaible = r_2.json().get("data", {}).get("cant_product", "No disponible")
+
+                text_index.value = f"Producto {index}"
+                text_name.value = f"Name: {name}"
+                text_code.value = f"Codigo: {code}"
+                text_price.value = f"Precio: {price}"
+                text_description.value = f"descripcion: {description}"
+                text_cant_avaible.value = f"Cantidad disponible: {cant_avaible}"
+
+            page.update()
+
+        buttom_regret = ft.ElevatedButton(text="Regresar", on_click=on_regret)
+        buttom_next = ft.ElevatedButton(text="Siguiente", on_click=on_next)
+
+        row_butom = ft.Row([buttom_regret, buttom_next])
 
     buttom_create_order = ft.Container(content=ft.Text("Hacer un pedido"), margin=10,
                                        padding=10,
